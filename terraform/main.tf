58d81515ef8fa0095f01d43feb793b45ed31c8b3
devilmind116 <74051978+devilmind116@users.noreply.github.com>
2024-03-31 21:55:37 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -93,17 +93,14 @@ resource "aws_instance" "my_ec2_instance1" {
       "sudo yum install git -y",
 
       # Install Jenkins
-      "sudo apt update -y",
-      "wget -O - https://packages.adoptium.net/artifactory/api/gpg/key/public | tee /etc/apt/keyrings/adoptium.asc",
-      "sudo apt update -y",
-      "sudo apt install temurin-17-jdk -y",
-      "/usr/bin/java --version",
-      "curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null",
-      "echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null",
-      "sudo apt-get update -y",
-      "sudo apt-get install jenkins -y",
+      "sudo yum update -y",
+      "sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo",
+      "sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key",
+      "sudo yum upgrade",
+      "sudo dnf install java-17-amazon-corretto -y",
+      "sudo yum install jenkins -y",
+      "sudo systemctl enable jenkins",
       "sudo systemctl start jenkins",
-      "sudo systemctl status jenkins",
 
       # Install Docker
       # REF: https://docs.aws.amazon.com/serverless-application-model/latest/developerguide/install-docker.html
